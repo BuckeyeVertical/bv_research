7871ce2c73126ac4514ebc11791c1ed83c1c1b30
EashanVytla <Eashan.Vytla@gmail.com>
2025-05-17 14:52:11 -0400

    Sanity checked worked... going back to real image

--- DinoDetr/rf_detr_crop.py
+++ DinoDetr/rf_detr_crop.py
@@ -117,9 +117,9 @@ def process_image_with_tiles(model, image, tile_size=728, overlap=100, threshold
 model = RFDETRLarge(resolution=728)
 
 
-# image = Image.open("data/b_50_frames/frame_000960.jpg").convert("RGB")
-url = "https://media.roboflow.com/notebooks/examples/dog-2.jpeg"
-image = Image.open(io.BytesIO(requests.get(url).content))
+image = Image.open("data/b_50_frames/frame_000960.jpg")
+# url = "https://media.roboflow.com/notebooks/examples/dog-2.jpeg"
+# image = Image.open(io.BytesIO(requests.get(url).content))
 # Remove the undefined crop variable check
 # Process with tiling directly
 detections = process_image_with_tiles(model, image, tile_size=728, overlap=200, threshold=0.5)
