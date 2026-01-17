ba5ba5329f5bd45d6a39819718e9098e331764ac
Trevor Clute <tclute07@outlook.com>
2026-01-17 15:01:07 -0500

    Update enhance_resolution.py

--- DataGenPipeline/steps/enhance_resolution.py
+++ DataGenPipeline/steps/enhance_resolution.py
@@ -2,6 +2,7 @@ from steps.utils import image_step
 
 @image_step
 def run(image):
+    # image is a np.ndarray
     # TODO: artificially enhance the image resolution of the image
     return image
 
